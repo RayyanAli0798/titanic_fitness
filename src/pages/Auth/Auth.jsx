a82bb6f3d6ceef0a24bdbef78cb74d7a860f7f5f
RayyanAli0798 <ali.rayyan0798@gmail.com>
2025-11-04 16:57:50 +0000

    MobileHeader

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,9 +1,70 @@
 import "./Auth.css"
+import HeroBannner from "../../components/HeroBanner/HeroBanner"
+import AuthImage1 from "../../assets/AuthBanner_01.png"
+import { useState } from "react"
+
+
+
+function LoginForm({ toggle }) {
+    return (
+        <form className="authForm">
+            
+            <h1 className="AuthTitle"> Unsinkable Gains await </h1>
+            <div className="InputBoxsForAuthPage">
+                <input className="InputBoxForAuth" type="email" minLength={12} required placeholder="Email" />
+                <input className="InputBoxForAuth" type="text" minLength={8} required placeholder="Password" />
+                
+            </div>
+
+            <div className="ButtonsArea">
+                <button className="ProceedBtnForAuthPage"> LOGIN </button>
+                <div className="smallTextAreaAuthPage">
+                    <p> Dont have an account?</p>
+                    <button onClick={toggle} className="ChangeBtn"> Register </button>
+                </div>
+
+            </div>
+        </form>
+    )
+}
+function RegisterForm({ toggle }) {
+    return (
+        <form className="authForm">
+
+            <h1 className="AuthTitle"> Unsinkable Gains await </h1>
+            <div className="InputBoxsForAuthPage">
+                <input className="InputBoxForAuth" type="email" minLength={12} required placeholder="Email" />
+                <input className="InputBoxForAuth" type="text" minLength={8} required placeholder="Password" />
+                <input className="InputBoxForAuth" type="text" minLength={3} required placeholder="Display Name" />
+            </div>
+
+            <div className="ButtonsArea">
+                <button className="ProceedBtnForAuthPage"> REGISTER NOW </button>
+                <div className="smallTextAreaAuthPage">
+                    <p> Already have an account?</p>
+                    <button className="ChangeBtn" onClick={toggle}> Login  </button>
+                </div>
+
+            </div>
+        </form>
+    )
+}
+
 
 export default function Auth() {
-    return(
-        <>
-            auth page
-        </>
+    const [registerForm, setRegisterForm] = useState(true)
+    function toggleForm() {
+        setRegisterForm(!registerForm);
+    }
+
+
+    return (
+        <div className="authLayout">
+            <HeroBannner image={AuthImage1} />
+            <div className="AuthFormColumn">
+                {registerForm ? <RegisterForm toggle={toggleForm} /> : <LoginForm toggle={toggleForm} />}
+
+            </div>
+        </div>
     )
 }
